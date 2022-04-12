e2177758f918f41f3f7a9e98b2f829d3328cdeb5
HAK3LI <thehak3li@gmail.com>
2022-04-13 01:27:29 +0300

    Initial commit

---
+++ My project/Assets/Data.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Data : MonoBehaviour
+{
+    public int number;
+
+    public int getNumber()
+    {
+        return number;
+    }
+
+    public void setNumber(int value)
+    {
+        number = value;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
